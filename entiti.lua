0b800ac843a84e8fb2c8a04645c3ea44a9976cc3
tezcan185 <48596330+tezcan185@users.noreply.github.com>
2019-03-31 23:30:37 +0300

    Create entiti.lua

---
+++ entiti.lua
@@ -0,0 +1,12 @@
+--[[
+    Generated using: DarkRP | Entity Generator
+    https://csite.io/tools/gmod-darkrp-entity
+--]]
+DarkRP.createEntity("Sigara", {
+    ent = "weapon_vape_golden",
+    model = "weapon_vape_golden.mdl",
+    price = 1000,
+    max = ,
+    cmd = "buysigara",
+    category = "Sigaralar"
+})
